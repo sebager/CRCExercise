878a6655344c8783a5b9a6cfe390001a128529f6
sebager <sebager764@student.polsl.pl>
2019-04-23 13:57:54 +0200

    added book repository

---
+++ src/DotNetCoreWebAPI/Repository/BookRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DotNetCoreWebAPI.DbContexts;
+using DotNetCoreWebAPI.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetCoreWebAPI.Repository
+{
+    public class BookRepository : IBookRepository<Book>
+    {
+        private readonly BookContext _dbContext;
+
+        public BookRepository(BookContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+        public async Task<IEnumerable<Book>> GetAll()
+        {
+            return await _dbContext.Books.ToListAsync();
+        }
+
+
+        public async Task<Book> Get(long id)
+        {
+            return await _dbContext.Books.FirstOrDefaultAsync(b => b.Id == id);
+        }
+
+
+        public async Task Add(Book entity)
+        {
+            await _dbContext.Books.AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task Update(Book book, Book entity)
+        {
+            book.Name = entity.Name;
+            book.Author = entity.Author;
+            book.ISBN = entity.ISBN;
+            book.Published = entity.Published;
+
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task Delete(Book book)
+        {
+            _dbContext.Remove(book);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ src/DotNetCoreWebAPI/Repository/IBookRepository.cs
@@ -0,0 +1,15 @@
+﻿using DotNetCoreWebAPI.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DotNetCoreWebAPI.Repository
+{
+    public interface IBookRepository<TEntity>
+    {
+        Task<IEnumerable<TEntity>> GetAll();
+        Task<TEntity> Get(long id);
+        Task Add(TEntity entity);
+        Task Update(Book book, TEntity entity);
+        Task Delete(Book book);
+    }
+}
