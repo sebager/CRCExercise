f15ed31e5d83da9ffb7914101745a4f34d1f6f31
sebager <sebager764@student.polsl.pl>
2019-04-23 12:51:47 +0200

    addded book db context

---
+++ src/DotNetCoreWebAPI/DbContexts/BookContext.cs
@@ -0,0 +1,14 @@
+﻿using DotNetCoreWebAPI.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetCoreWebAPI.DbContexts
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
