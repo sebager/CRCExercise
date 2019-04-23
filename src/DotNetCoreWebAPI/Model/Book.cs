1ce7396b6717fa7aa165b8271458dd8afdcbe094
sebager <sebager764@student.polsl.pl>
2019-04-23 12:46:05 +0200

    added book model

---
+++ src/DotNetCoreWebAPI/Model/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DotNetCoreWebAPI.Model
+{
+    public class Book
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        [Required]
+        [StringLength(13)]
+        public string ISBN { get; set; }
+
+
+        [Required]
+        public DateTime Published { get; set; }
+    }
+}
